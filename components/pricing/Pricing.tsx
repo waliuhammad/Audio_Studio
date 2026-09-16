379a91e7fa7e24a3ddeebb53ffaf0eeab35e5d05
Zainab Irfan <huzainab109@gmail.com>
2026-09-16 17:28:46 +0500

    Reduce spacing between FAQ section

--- components/pricing/Pricing.tsx
+++ components/pricing/Pricing.tsx
@@ -58,15 +58,15 @@ export function Pricing() {
       id="pricing"
       className="
         container-studio
-       scroll-mt-32
-pb-14
-pt-0
-sm:scroll-mt-40
-sm:pb-18
-sm:pt-2
-lg:scroll-mt-44
-lg:pb-24
-lg:pt-3
+        scroll-mt-32
+        pb-4
+        pt-0
+        sm:scroll-mt-40
+        sm:pb-6
+        sm:pt-2
+        lg:scroll-mt-44
+        lg:pb-8
+        lg:pt-3
       "
     >
       {/* ================================================= */}
